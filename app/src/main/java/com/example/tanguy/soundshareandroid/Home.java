9465418d2ee388ac4b80c3380de05c855e78ade0
Unknown <tanguy.moullec@gmail.com>
2018-06-19 02:33:37 +0200

    Late night commit

--- app/src/main/java/com/example/tanguy/soundshareandroid/Home.java
+++ app/src/main/java/com/example/tanguy/soundshareandroid/Home.java
@@ -1,20 +1,64 @@
 package com.example.tanguy.soundshareandroid;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class Home extends AppCompatActivity {
-    private FirebaseAuth firebaseAuth;
 
+    private FirebaseAuth firebaseAuth;
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-    }
+
+        ArrayList<String> songList = new ArrayList<>();
+
+        songList.add("poudlard");
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        recyclerView.setLayoutManager(new LinearLayoutManager(Home.this));
+        adapter = new MyRecyclerViewAdapter(Home.this, songList);
+        //adapter.setClickListener (this);
+        recyclerView.setAdapter(adapter);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("songs")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TESTFIRESTORE", document.getId() + " => " + document.getData());
+                            }
+
+                            Log.d("SONG REQUEST", "playlist pulled successfully");
+
+
+                        } else {
+                            Log.w("SONG REQUEST", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        }
 
     @Override
     public void onBackPressed(){
--- app/src/main/java/com/example/tanguy/soundshareandroid/MainActivity.java
+++ app/src/main/java/com/example/tanguy/soundshareandroid/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.tanguy.soundshareandroid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
