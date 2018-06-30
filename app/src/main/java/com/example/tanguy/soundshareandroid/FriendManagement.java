63b4e78ad23aa4f3b5c64acc6daf84a44954246c
Unknown <tanguy.moullec@gmail.com>
2018-06-30 21:44:17 +0200

    FriendManagement

---
+++ app/src/main/java/com/example/tanguy/soundshareandroid/FriendManagement.java
@@ -0,0 +1,13 @@
+package com.example.tanguy.soundshareandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FriendManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friend_management);
+    }
+}
