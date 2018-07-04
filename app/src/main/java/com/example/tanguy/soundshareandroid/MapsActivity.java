9105328853528870b75f7cffa78ccf7847df95bf
LiyangSun <liyang.patricksun@gmail.com>
2018-07-04 11:33:51 +0200

    Working friend localisation

--- app/src/main/java/com/example/tanguy/soundshareandroid/MapsActivity.java
+++ app/src/main/java/com/example/tanguy/soundshareandroid/MapsActivity.java
@@ -31,6 +31,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.os.Build.ID;
+
 /**
  * Map page for localising the user and its friends' positions
  */
@@ -39,6 +41,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private FriendManagement friendManager = new FriendManagement();
     private ArrayList<Friend> friendList = new ArrayList<>();
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,106 +58,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         GPSTracker gps = new GPSTracker(MapsActivity.this);
         if(gps.canGetLocation()){
-//            Location location = gps.getLocation();
             double latitude = gps.getLatitude();
             double longitude = gps.getLongitude();
-//            Location location = gps.getLocation();
-//            double latitude = location.getLatitude();
-//            double longitude= location.getLongitude();
-            LatLng pos = new LatLng(latitude, longitude);
+            LatLng mpos = new LatLng(latitude, longitude);
             int height = 100;
             int width = 100;
             BitmapDrawable bitmapdraw=(BitmapDrawable)getResources().getDrawable(R.drawable.logo_miniature);
             Bitmap b=bitmapdraw.getBitmap();
             Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
-            mMap.addMarker(new MarkerOptions().position(pos).title("I'm here !").icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
+            mMap.addMarker(new MarkerOptions().position(mpos).title("I'm here !").icon(BitmapDescriptorFactory.fromBitmap(smallMarker)));
 //            float zoomLevel = 16.0f;
-            Map<String, Object> positionsFriends = this.getPositionsFriends();
-            ArrayList<Double> latitudes = (ArrayList<Double>) positionsFriends.get("latitudes");
-            ArrayList<Double> longitudes = (ArrayList<Double>) positionsFriends.get("longitudes");
-            mMap.addMarker(new MarkerOptions().position(new LatLng(40, 40)));
-            if(latitudes.size()>0){
-                for (int i = 0; i<latitudes.size(); i++){
-                    double lat = latitudes.get(i);
-                    double lon = longitudes.get(i);
-                    LatLng position = new LatLng(lat, lon);
-                    mMap.addMarker(new MarkerOptions().position(position).title(this.friendManager.getFriendList().get(i).getUsername()));
-                }
-            }
-//            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, zoomLevel));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(pos));
-            Toast.makeText(getApplicationContext(), "Votre position est  \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-        }else{
-            gps.showSettingsAlert();
-        }
-    }
+//            Map<String, Object> positionsFriends = this.getPositionsFriends();
+//            Map<String, Object> pos = new HashMap<>();
+//            final ArrayList<Double> lat = new ArrayList<>();
+//            final ArrayList<Double> lon = new ArrayList<>();
 
-    public Map<String, Object> getPositionsFriends(){
-        Map<String, Object> pos = new HashMap<>();
-        final ArrayList<Double> lat = new ArrayList<>();
-        final ArrayList<Double> lon = new ArrayList<>();
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
+            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+            FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+            if (currentUser == null) {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                final String userID = currentUser.getUid();
 
-//        friendManager.getFriends();
-
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
-        if (currentUser == null) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-        } else {
-            final String userID = currentUser.getUid();
-
-            // Get the list of user's friends from database
-            db.collection("users").document(userID).collection("friends")
-                    .get()
-                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                        @Override
-                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                            if (task.isSuccessful()) {
-                                for (QueryDocumentSnapshot document : task.getResult()) {
-                                    Map<String, Object> friendJson = document.getData();
-
-                                    String ID = document.getId();
-                                    String username = (String) friendJson.get("username");
-                                    String email = (String) friendJson.get("email");
-                                    Friend friend = new Friend(ID, email, username);
-                                    friendList.add(friend);
-                                    Log.e("CHECK_1", " " + friendList.size());
-                                }
-                            } else {
-                                Log.e("ERROR", "error getting friends" + task.getException());
-                            }
-                        }
-                    });
-//        ArrayList<Friend> friends = this.friendManager.getFriendList();
-            Log.e("CHECK_2", " " + friendList.size());
-            for (int i = 0; i < friendList.size(); i++) {
-                Friend friend = friendList.get(i);
-                String friendID = friend.getID();
-                db.collection("users").document(friendID).collection("position").get()
+                // Get the list of user's friends from database
+                db.collection("users").document(userID).collection("friends")
+                        .get()
                         .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                             @Override
                             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-
                                 if (task.isSuccessful()) {
                                     for (QueryDocumentSnapshot document : task.getResult()) {
-                                        Map<String, Object> positionJson = document.getData();
-                                        double latitude = (double) positionJson.get("latitude");
-                                        double longitude = (double) positionJson.get("longitude");
-                                        lat.add(latitude);
-                                        lon.add(longitude);
-                                        Log.e("GET LOCATIONS", "Succeeded");
+                                        Map<String, Object> friendJson = document.getData();
+
+                                        String ID = document.getId();
+                                        String username = (String) friendJson.get("username");
+                                        String email = (String) friendJson.get("email");
+                                        Friend friend = new Friend(ID, email, username);
+                                        friendList.add(friend);
+                                    }
+                                    Log.e("CHECK_1", " " + friendList.size());
+                                    for (int i = 0; i < friendList.size(); i++) {
+                                        Friend fr = friendList.get(i);
+                                        String friendID = fr.getID();
+                                        final String username = fr.getUsername();
+                                        db.collection("users").document(friendID).collection("position").get()
+                                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                    @Override
+                                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                                        if (task.isSuccessful()) {
+                                                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                                                Map<String, Object> positionJson = document.getData();
+                                                                double lat = (double) positionJson.get("latitude");
+                                                                double lon = (double) positionJson.get("longitude");
+                                                                LatLng position = new LatLng(lat, lon);
+                                                                mMap.addMarker(new MarkerOptions().position(position).title(username));
+//                                                                lat.add(lat);
+//                                                                lon.add(lon);
+                                                                Log.e("GET LOCATIONS", "Succeeded");
+                                                            }
+                                                        } else {
+                                                            Log.e("ERROR", "error getting positions" + task.getException());
+                                                        }
+                                                    }
+                                                });
                                     }
                                 } else {
-                                    Log.e("ERROR", "error getting positions" + task.getException());
+                                    Log.e("ERROR", "error getting friends" + task.getException());
                                 }
                             }
                         });
             }
-            pos.put("latitudes", lat);
-            pos.put("longitudes", lon);
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(mpos));
+            Toast.makeText(getApplicationContext(), "Votre position est  \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+        }else{
+            gps.showSettingsAlert();
         }
-        return pos;
     }
 }
